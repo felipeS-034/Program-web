95e68b26402e2d56ac33aa66612ce030c85a0bab
felipeS-034 <andressantacruz268@gmail.com>
2023-08-17 20:14:21 -0500

    entrega taller contador

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {Header, Counter, Footer} from "./components"
+function App(){
+
+    return(
+        <>
+          <Header/>
+          <Counter />
+          <Footer />
+        </>
+    )
+}
+
+export default App
+
